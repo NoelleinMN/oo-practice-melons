5361c747c94a67a0549bf754718d2e0409dd850f
Noelle Notermann <noona020@umn.edu>
2021-03-11 17:03:20 +0000

    initialized superclass

--- harvest.py
+++ harvest.py
@@ -11,18 +11,23 @@ class MelonType(object):
         """Initialize a melon."""
 
         self.pairings = []
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
 
-        # Fill in the rest
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
 
-        # Fill in the rest
+        self.pairings.append(pairing)
+        # append a pairing to list of pairings
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
-        # Fill in the rest
+        self.code = new_code
 
 
 def make_melon_types():
